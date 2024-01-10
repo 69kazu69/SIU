85d845ff92dad3eebd9e9b4b1cd7f7fe7fcdbd85
Aanchal  Walia <aanchalwalia@Aanchals-MacBook-Air.local>
2024-01-10 19:36:43 +0530

    first

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,12 +6,13 @@ const Hero = () => {
   return (
     <header className='w-full flex justify-center items-center flex-col'>
       <nav className='flex justify-between items-center w-full mb-10 pt-3'>
-        <img src={logo} alt='sumz_logo' className='w-28 object-contain' />
-
+<p className="head_text">
+  SIU
+</p>
         <button
           type='button'
           onClick={() =>
-            window.open("https://github.com/TidbitsJS/Summize", "_blank")
+            window.open("https://github.com/69kazu69", "_blank")
           }
           className='black_btn'
         >
@@ -20,11 +21,11 @@ const Hero = () => {
       </nav>
 
       <h1 className='head_text'>
-        Summarize Articles with <br className='max-md:hidden' />
-        <span className='orange_gradient '>OpenAI GPT-4</span>
+        Sum It Up by<br className='max-md:hidden' />
+        <span className='orange_gradient '>Aanchal Walia</span>
       </h1>
       <h2 className='desc'>
-        Simplify your reading with Summize, an open-source article summarizer
+        An open-source article summarizer
         that transforms lengthy articles into clear and concise summaries
       </h2>
     </header>
--- src/services/article.js
+++ src/services/article.js
@@ -1,13 +1,12 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
-const rapidApiKey = import.meta.env.VITE_RAPID_API_ARTICLE_KEY;
-
+const Key = import.meta.env.VITE_KEY;
 export const articleApi = createApi({
     reducerPath: 'articleApi',
     baseQuery: fetchBaseQuery({
         baseUrl: 'https://article-extractor-and-summarizer.p.rapidapi.com/',
         prepareHeaders: (headers) => {
-            headers.set('X-RapidAPI-Key', rapidApiKey);
+            headers.set('X-RapidAPI-Key', Key);
             headers.set('X-RapidAPI-Host', 'article-extractor-and-summarizer.p.rapidapi.com');
 
             return headers;
